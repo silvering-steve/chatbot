0a1c0b70356c5e80a3be01e44ea157d7fa17b5cd
silvering-steve <sstevenadi@gmail.com>
2023-12-13 15:50:33 +0700

    adding personalities settings

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,39 +15,69 @@ def main():
         st.session_state.client = None
 
     # Side Bar Settings
-    st.sidebar.title("Settings")
+    settings_tab, personality_tab = st.sidebar.tabs(["Settings", "Personality"])
 
-    # Set the information needed
-    name = st.sidebar.text_input("Name", placeholder="Vincent")
-    personalities = st.sidebar.text_area("Personalities", placeholder="You are a helpful assistant bot")
+    # Settings Tab
+    with settings_tab:
+        st.title("Settings")
 
-    # Set horizontal columns
-    col1, col2 = st.sidebar.columns(2)
-    with col1:
-        model = st.selectbox(
-            "Model",
-            ("gpt-3.5-turbo-1106", "gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-instruct")
-        )
-    with col2:
-        lang = st.selectbox(
-            "Language",
-            ("Indonesia", "English")
-        )
+        api_key = st.text_input("Api Key", type="password")
 
-    api_key = st.sidebar.text_input("Api Key", type="password")
-
-    if st.sidebar.button("Chat", type="primary",):
-        match lang:
-            case "Indonesia":
-                intro = f"Namamu adalah {name}"
-            case "English":
-                intro = f"Your name is {name}"
-
-        st.session_state.messages = [
-            {"role": "system", "content": f"{intro}. {personalities}"}
-        ]
+        # Set horizontal columns
+        col2_1, col2_2 = st.columns(2)
+        with col2_1:
+            model = st.selectbox(
+                "Model",
+                ("gpt-3.5-turbo-1106", "gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-instruct")
+            )
+        with col2_2:
+            lang = st.selectbox(
+                "Language",
+                ("Indonesia", "English"),
+                placeholder="Indonesia"
+            )
 
         st.session_state.client = OpenAI(api_key=api_key)
+
+    # Personality Tab
+    with personality_tab:
+        st.title("Personality")
+
+        # Set the information needed
+        name = st.text_input("Name", placeholder="Vincent")
+
+        col1_1, col1_2 = st.columns(2)
+        with col1_1:
+            gender = st.selectbox(
+                "Gender",
+                ("Laki - Laki", "Perempuan"),
+                placeholder="Laki - Laki"
+            )
+        with col1_2:
+            age = st.text_input("Age", placeholder="20")
+
+        city = st.text_input("City", placeholder="Jakarta")
+        personality = st.text_area("Personality", placeholder="Kind, Talktive, ...", max_chars=50)
+        behaviour = st.text_area("Behaviour", placeholder="Usually said ooh at the end of the text", max_chars=50)
+        like = st.text_area("Like", placeholder="Food, Golf, ...", max_chars=50)
+        dislike = st.text_area("Dislike", placeholder="Bad People, Spicy Food", max_chars=50)
+
+        personalities = f"""Kamu adalah {name} teman {gender}ku yang berumur {age} dan berasal dari {city}. Kamu memiliki
+                kepribadian {personality}. Kamu kalau berbicara biasanya {behaviour}. Kamu menyukai {like} dan kamu tidak menyukai
+                {dislike}. Kamu tidak boleh sama sekali mengaku bahwa kamu chatbot ataupun AI. Kamu biasanya yang mengusulkan
+                topik pembicaraan terlebih dahulu dan tidak pernah menggunakan kalimat tanya yang baku seperti 'ada yang ingin
+                ditanyakan' atau 'ada yang bisa dibantu'."""
+
+        if st.button("Chat", type="primary", ):
+            match lang:
+                case "Indonesia":
+                    intro = f"Namamu adalah {name}"
+                case "English":
+                    intro = f"Your name is {name}"
+
+            st.session_state.messages = [
+                {"role": "system", "content": f"{intro}. {personalities}"}
+            ]
 
     # View and update chat
     for message in st.session_state.messages:
